27380dad61b7ab1e5d67a38728a1182989e8480a
_ahmad_zaza <ahmad@voila.digital>
2022-08-05 17:05:44 +0300

    fix-send-subscribtion

--- app/Services/RetailDealerServices/SubscriptionService.php
+++ app/Services/RetailDealerServices/SubscriptionService.php
@@ -42,47 +42,50 @@ class SubscriptionService
         $subRequest['status'] = Config::get('constants.retailDealer.subscribeRequest.pending');
         $subRequest['retail-dealer'] = $subRequest->retailDealer()->get();
         $retail_dealer = RetailDealer::find($request->retail_dealer_id);
-       /////////////////////////////// send notifications
-       $title = "You have new subscribe request!";
-       $body = "A new subscribe request from " . $retail_dealer->first_name  . " " . $retail_dealer->last_name . "";
-       $type = "subscribe";
+        /////////////////////////////// send notifications
+        $title = "You have new subscribe request!";
+        $body = "A new subscribe request from " . $retail_dealer->first_name  . " " . $retail_dealer->last_name . "";
+        $type = "subscribe";
 
-       $admins = CompanyUser::where('company_id', $request->company_id)
-       ->where('user_type', Config::get('constants.company.users.admin_type'))->get();
-    //    return response()->json(["admins" => $admins]);
-       foreach($admins as $admin){
-        //    (new static)->sendNotification($admin->firebasetoken, $title, $body,  $type, $admin->id); //send notification
-        $data = [
-            "registration_ids" => [$admin->firebasetoken],
-            "notification" => [
-                "body"  => $body,
-                "title" => $title,
-            ],
-            "data" => [
-                "type" => $type,
-                "id" => $request->retail_dealer_id
-            ],
-        ];
+        $admins = CompanyUser::where('company_id', $request->company_id)
+            ->where('user_type', Config::get('constants.company.users.admin_type'))->get();
+        //    return response()->json(["admins" => $admins]);
+        foreach ($admins as $admin) {
+            //    (new static)->sendNotification($admin->firebasetoken, $title, $body,  $type, $admin->id); //send notification
+            $data = [
+                "registration_ids" => [$admin->firebasetoken],
+                "notification" => [
+                    "body"  => $body,
+                    "title" => $title,
+                ],
+                "data" => [
+                    "type" => $type,
+                    "id" => $request->retail_dealer_id
+                ],
+            ];
 
-        $ch = curl_init();
+            $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_POST, 1);
 
-        $headers = array();
-        $headers[] = 'Content-Type: application/json';
-        $headers[] = 'Authorization: key=AAAAxbkUDBc:APA91bHL9Z4tWphs2HKNWJ4D9EUcinadhgW2BHCVfrkDPtkhOXMM8Z1QzyZSjuJzh8TiAsChM0rTIAa2ri35SJwjESmZO5A-Oi3a8TssSpNWNhVPzFJg9kVzYgw7jNn7RPRP8G6rkuUd';
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-           $notification = new notifications();
-           $notification->user_id = $admin->id;
-           $notification->title = $title;
-           $notification->body = $body;
-           $notification->type = "Dealer";
-           $notification->save();
-       }
-       ////////////////////////////////////////////
+            $headers = array();
+            $headers[] = 'Content-Type: application/json';
+            $headers[] = 'Authorization: key=AAAAxbkUDBc:APA91bHL9Z4tWphs2HKNWJ4D9EUcinadhgW2BHCVfrkDPtkhOXMM8Z1QzyZSjuJzh8TiAsChM0rTIAa2ri35SJwjESmZO5A-Oi3a8TssSpNWNhVPzFJg9kVzYgw7jNn7RPRP8G6rkuUd';
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            $result = curl_exec($ch);
+
+
+            $notification = new notifications();
+            $notification->user_id = $admin->id;
+            $notification->title = $title;
+            $notification->body = $body;
+            $notification->type = "Dealer";
+            $notification->save();
+        }
+        ////////////////////////////////////////////
 
         return (new static)->successMessage($subRequest, '200');
     }
